ed98cf6cf2a120d96d87515d8536ec2aada8ee7b
Umar Sani <umarsani361@gmail.com>
2024-06-11 05:00:01 +0700

    update functionalities in pentashih page

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  runtimeConfig: {
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseKey: process.env.SUPABASE_KEY,
+  },
+  css: [
+    'primevue/resources/themes/aura-light-green/theme.css',
+    '@/assets/css/tailwind.css',
+  ],
+  modules: ["nuxt-primevue", '@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false,
+  },
+  primevue: {
+    options: {
+      ripple: true,
+      inputStyle: 'filled',
+      unstyled: true,
+      pt: {
+        tabview: {
+          tabpanel: {
+            header: () => ({
+              class: [
+                'bg-black',
+              ]
+            }),
+          }
+        }
+      },
+    },
+    importPT: { as: 'Aura', from: '~/presets/aura' }
+  },
+})
---
+++ server/api/subject.get.js
@@ -0,0 +1,12 @@
+// import { supabase } from '@/utils/supabase'
+
+// export default eventHandler(async (event) => {
+
+//   if (supabase) {
+//     const { data } = await supabase.from('subject').select('*')
+//     return { data }    
+//   } else {
+//     return { data: "error" }
+//   }
+
+// })
